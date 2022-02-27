4f161538e320655234576800f455a92b2e19042b
Dubkov <yevgeniydubkov@gmail.com>
2022-02-28 01:54:41 +0300

    lesson7

--- HW3/client.py
+++ HW3/client.py
@@ -13,10 +13,43 @@ from common.utils import send_msg, get_msg
 from json import JSONDecodeError
 import logging
 import log.client_log_config
+import argparse
+from decos import log
 
 CLIENT_LOGGER = logging.getLogger('client')
 
 
+@log
+def msg_from_server(msg):
+    if ACTION in msg and msg[ACTION] == MSG and \
+            SENDER in msg and MSG_TEXT in msg:
+        print(f'Получено сообщение от пользователя '
+              f'{msg[SENDER]}:\n{msg[MSG_TEXT]}')
+        CLIENT_LOGGER.info(f'Получено сообщение от пользователя '
+                    f'{msg[SENDER]}:\n{msg[MSG_TEXT]}')
+    else:
+        CLIENT_LOGGER.error(f'Получено некорректное сообщение с сервера: {msg}')
+
+
+@log
+def create_msg(sock, account_name='Guest'):
+    msg = input('Введите текст для отправки или для завершения работы: \'777\'  ')
+    if msg == '777':
+        sock.close()
+        CLIENT_LOGGER.info('Завершение работы по команде пользователя.')
+        print('!!!Спасибо за использование нашего сервиса!!!')
+        exit(0)
+    msg_dict = {
+        ACTION: MSG,
+        TIME: time(),
+        ACCOUNT_NAME: account_name,
+        MSG_TEXT: msg
+    }
+    CLIENT_LOGGER.debug(f'сообщение: {msg_dict}')
+    return msg_dict
+
+
+@log
 def create_presence(account_name='Guest'):
     out = {
         ACTION: PRESENCE,
@@ -30,6 +63,7 @@ def create_presence(account_name='Guest'):
     return out
 
 
+@log
 def process_ans(msg):
     CLIENT_LOGGER.debug(f'Разбор сообщения от сервера : {msg}')
     if RESPONSE in msg:
@@ -43,13 +77,19 @@ def main():
     try:
         server_address = argv[1]
         server_port = int(argv[2])
+        client_mode = argv[3]
         if server_port < 1024 or server_port > 65535:
             raise ValueError
+        if client_mode not in ('listen', 'send'):
+            exit(1)
     except IndexError:
         server_address = DEFAULT_IP_ADDRESS
         server_port = DEFAULT_PORT
+        client_mode = ('-m', '--mode')
     CLIENT_LOGGER.info(f'Запущен клиент с параметрами: '
                        f'адрес сервера : {server_address}, порт : {server_port}')
+    CLIENT_LOGGER.critical(f'Указан недопустимый режим работы {client_mode}, '
+                    f'допустимые режимы: listen , send')
     transport = socket(AF_INET, SOCK_STREAM)
     transport.connect((server_address, server_port))
     msg_to_server = create_presence()
@@ -57,11 +97,32 @@ def main():
     try:
         answer = process_ans(get_msg(transport))
         CLIENT_LOGGER.info(f'Принят ответ от сервера: {answer}')
+        exit(1)
     except JSONDecodeError:
         CLIENT_LOGGER.error(f'Не удалось декодировать Json строку')
+        exit(1)
     except ConnectionRefusedError:
         CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}')
+        exit(1)
+    else:
+        if client_mode == 'send':
+            print('Режим работы - отправка сообщений.')
+        else:
+            print('Режим работы - приём сообщений.')
+        while True:
+            if client_mode == 'send':
+                try:
+                    send_msg(transport, create_msg(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
+                    exit(1)
 
+            if client_mode == 'listen':
+                try:
+                    msg_from_server(get_msg(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    CLIENT_LOGGER.error(f'Соединение с сервером {server_address} было потеряно.')
+                    exit(1)
 
 if __name__ == '__main__':
     main()
--- HW3/server.py
+++ HW3/server.py
@@ -12,20 +12,29 @@ from common.utils import send_msg, get_msg
 from json import JSONDecodeError
 import logging
 import log.server_log_config
+from decos import log
+from select import select
+from time import time
 
 SERVER_LOGGER = logging.getLogger('server')
 
-
-def process_client_msg(msg):
+@log
+def process_client_msg(msg, msg_list, client):
     SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {msg}')
     if ACTION in msg and msg[ACTION] == PRESENCE and TIME in msg \
             and USER in msg and msg[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
-
+        send_msg({RESPONSE: 200})
+        return
+    elif ACTION in msg and msg[ACTION] == MSG and \
+            TIME in msg and MSG_TEXT in msg:
+        msg_list.append((msg[ACCOUNT_NAME], msg[MSG_TEXT]))
+        return
+    else:
+        send_msg(client, {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        })
+        return
 
 def main():
     try:
@@ -58,23 +67,57 @@ def main():
 
     transport = socket(AF_INET, SOCK_STREAM)
     transport.bind((listen_address, listen_port))
+    transport.settimeout(0.5)
+    clients = []
+    msgs = []
     transport.listen(MAX_CONNECTIONS)
 
     while True:
-        client, client_address = transport.accept()
-        SERVER_LOGGER.info(f'Установлено соединение с ПК : {client_address}')
         try:
-            msg_from_client = get_msg(client)
-            SERVER_LOGGER.debug(f'Получено сообщение от клиента : {msg_from_client}')
-            response = process_client_msg(msg_from_client)
-            SERVER_LOGGER.info(f'Сформирован ответ клиенту : {response}')
-            send_msg(client, response)
-            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается')
-            client.close()
-        except JSONDecodeError:
-            SERVER_LOGGER.error(f'Не удалось декодировать Json строку, полученную от'
-                                f'клиента {client_address}. Соединение закрывается')
-            client.close()
+            client, client_address = transport.accept()
+        except OSError as err:
+            print(err.errno)
+            pass
+        else:
+            SERVER_LOGGER.info(f'Установлено соединение с ПК : {client_address}')
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        if recv_data_lst:
+            for client_with_msg in recv_data_lst:
+                try:
+                    process_client_msg(get_msg(client_with_msg),
+                                       msgs, client_with_msg)
+                except:
+                    SERVER_LOGGER.info(f'Клиент {client_with_msg.getpeername()} '
+                                f'отключился от сервера.')
+                    clients.remove(client_with_msg)
+
+        if msgs and send_data_lst:
+            message = {
+                ACTION: MSG,
+                SENDER: msgs[0][0],
+                TIME: time(),
+                MSG_TEXT: msgs[0][1]
+            }
+            del msgs[0]
+            for waiting_client in send_data_lst:
+                try:
+                    send_msg(waiting_client, message)
+                except:
+                    SERVER_LOGGER.info(f'Клиент {waiting_client.getpeername()}'
+                                       f' отключился от сервера.')
+                    waiting_client.close()
+                    clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
