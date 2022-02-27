e9a890b4d07152ae4a2fcc9957aab937fc3ec541
Dubkov <yevgeniydubkov@gmail.com>
2022-02-27 17:01:33 +0300

    lesson5 done

--- HW3/log/server_log_config.py
+++ HW3/log/server_log_config.py
@@ -6,11 +6,11 @@ from dateutil.utils import today
 
 log = logging.getLogger('server')
 PATH = os.path.dirname(os.path.abspath(__file__))
-PATH_DAILY_HAND = os.path.join(PATH, f'server{today().strftime("%d-%m-%Y")}.log')
+# PATH_DAILY_HAND = os.path.join(PATH, f'server{today().strftime("%d-%m-%Y")}.log')
 PATH = os.path.join(PATH, 'server.log')
 file_hand = logging.FileHandler(PATH, encoding='utf-8')
 
-file_hand_daily = handlers.TimedRotatingFileHandler(PATH_DAILY_HAND, when='D', interval=1, encoding='utf-8')
+file_hand_daily = handlers.TimedRotatingFileHandler(PATH, when='D', interval=1, encoding='utf-8')
 
 formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
 file_hand.setFormatter(formatter)
